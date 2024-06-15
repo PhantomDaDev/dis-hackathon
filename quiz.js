5224a87af1318a6292c6672af3185bd430768074
GitDpro <147998871+GitDpro@users.noreply.github.com>
2024-06-15 13:21:53 +0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -4,5 +4,68 @@
 // 60% done only due to bug in vs code
 
 
+if (typeof document !== "undefined") {
+  let element = document.querySelector(".class-name");
+
+  // Manipulating the DOM here
+}
+const quizData = [
+  {
+    question: "What would be the result of 3+2+”7″? ",
+    options: ["12", "7", "327", "57"],
+    answer: "57",
+  },
+  {
+    question: "What would be the result of 3+2+”7″? ",
+    options: ["12", "7", "327", "57"],
+    answer: "57",
+  },
+];
+
+const questionDiv = document.getElementById("questions");
+const optionsDiv = document.getElementById("options");
+const submitButton = document.getElementById("submit");
+
+let currentQuestion = 0;
+let score = 0;
+
+function makeQuestion() {
+  const question = quizData[currentQuestion];
+  questionDiv.innerText = question.question;
+
+  questionDiv.innerHTML = "";
+  question.options.forEach((option) => {
+    const button = document.createElement("element");
+    button.innerText = option;
+    optionsDiv.appendChild(button);
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const answer = quizData[currentQuestion].answer;
+
+  if (selectedButton.innerText === answer) {
+    score++;
+  }
+
+  currentQuestion++;
+
+  if (currentQuestion > quizData.length) {
+    makeQuestion;
+  } else {
+    showResult;
+  }
+}
+
+function showResult() {
+  quize.innerHTML = `
+    <h1>You have completed the quiz</h1>
+    <p>Your score is ${score}/${quizData.length}</p>
+    `;
+}
+
+makeQuestion;
 
 
