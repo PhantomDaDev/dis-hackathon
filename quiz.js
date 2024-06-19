931432a00b2b6f0638c545a0f2eb8a7033c133de
GitDpro <147998871+GitDpro@users.noreply.github.com>
2024-06-19 19:29:56 +0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -104,7 +104,7 @@ function selectAnswer(e) {
     nextQuestion("correct");
   } else {
     reasonDiv.innerHTML = `Wrong! ${quizData[currentQuestion].reason}`;
-     document.getElementById('cScore').innerText = `${score}/${quizData.length}`
+    document.getElementById('cScore').innerText = `${score}/${quizData.length}`
     nextQuestion("wrong");
   }
 
